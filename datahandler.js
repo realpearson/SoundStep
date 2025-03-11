32eb397b88e15cbf8651e6c86a4527b7c5721d8f
realpearson <david@wearemachinemedia.com>
2025-03-11 11:05:02 +0100

    changes thresh

--- datahandler.js
+++ datahandler.js
@@ -92,7 +92,7 @@ function createSession(name){
 //---------------------------------Data Processing & Analyzers-------------------------------------
 
 const defaultPeakSettings = {
-  framesUntilPeakConfirm: 4,
+  framesUntilPeakConfirm: 2,
   frameCooldownThresh: 10,
   hiMode: {peakThresh: 5, resetThresh: 1},
   loMode: {peakThresh: -5, resetThresh: -1},
