b5227cc5f3d83e3eb8f06b91047852655ec6bff9
Oskar Lövgren <oskarlov@kth.se>
2026-03-20 13:12:21 +0100

    kick test

--- src/app/presets/presets.js
+++ src/app/presets/presets.js
@@ -336,14 +336,15 @@ function createRunMusicSimulatorSession(){
 
         return function(){
    
+            console.log("kick updated")
             if(counter == 1){drums.playSpecific(0);}
-            if(counter == 3){drums.playSpecific(1);}
+            if(counter == 3){drums.playSpecific(0);}
             if(counter == 6){drums.playSpecific(0);}
-            if(counter == 7){drums.playSpecific(1);}
-            if(counter == 11){drums.playSpecific(1);}
+            if(counter == 7){drums.playSpecific(0);}
+            if(counter == 11){drums.playSpecific(0);}
             if(counter == 12){drums.playSpecific(0);}
             if(counter == 14){drums.playSpecific(0);}
-            if(counter == 15){drums.playSpecific(1);}
+            if(counter == 15){drums.playSpecific(0);}
 
 
       
