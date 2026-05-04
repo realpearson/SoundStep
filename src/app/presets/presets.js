ee9e40b22bc845d6760ecc8f76fa4573e23b00c2
Oskar Lövgren <oskarlov@kth.se>
2026-05-04 22:00:07 +0200

    control condition

--- src/app/presets/presets.js
+++ src/app/presets/presets.js
@@ -440,3 +440,89 @@ function createRunMusicSimulatorSession(){
     }
 }
 
+
+function createControlSimulatorSession(){
+    //Setup Sounds
+    
+
+    
+    //Listeners
+    const stepListeners = {
+        onLoPeakEvents: [
+            
+          
+        ]
+    }
+
+    const zXListeners = [
+
+    ]
+    
+    //....
+
+    //Processors
+    const peakXProcessor = createPeakAnalyzer(defaultPeakSettings, stepListeners);
+    //const peakYProcessor = createPeakAnalyzer(defaultPeakSettings, null);
+    //const peakZProcessor = createPeakAnalyzer(defaultPeakSettings, null);
+    const zeroXingProcessor = createZeroCrossingAnalyzer(defaultZeroCrossingSettings, zXListeners);
+    
+
+    //Processor Array
+    const testProcessorArr = [
+        {
+            processor: peakXProcessor, 
+            processorType: LOW_LEVEL_PROCESSOR_TYPES.PEAK, 
+            sensorType: SENSOR_TYPES.ACCELERATION,  
+            dataType: DATA_TYPES.X
+        },
+        ////{processor: rawYProcessor, processorType: LOW_LEVEL_PROCESSOR_TYPES.PEAK, sensorType: SENSOR_TYPES.ACCELERATION, dataType: DATA_TYPES.Y},
+        //{processor: rawRotZProcessor, processorType: "?", sensorType: SENSOR_TYPES.ROTATION, dataType: DATA_TYPES.Z},
+        //{processor: peakYProcessor, processorType: LOW_LEVEL_PROCESSOR_TYPES.PEAK, sensorType: SENSOR_TYPES.ACCELERATION, dataType: DATA_TYPES.Y},
+        //{processor: peakZProcessor, processorType: LOW_LEVEL_PROCESSOR_TYPES.PEAK, sensorType: SENSOR_TYPES.ACCELERATION, dataType: DATA_TYPES.Z},
+        {
+            processor: zeroXingProcessor, 
+            processorType: LOW_LEVEL_PROCESSOR_TYPES.ZERO_CROSSING, 
+            sensorType: SENSOR_TYPES.ACCELERATION, 
+            dataType: DATA_TYPES.X
+        }
+    ]
+
+
+    //These need to get called in both simulator and runrecorder!!!!
+    function onActivate(){
+
+    }
+
+    function onDeactivate(){
+    
+    }
+
+    //Simulator Rendering
+    function render(rawData){
+        renderRawData(rawData, [SENSOR_TYPES.ACCELERATION, DATA_TYPES.X], 0.5, 60, "Vertical Accel");
+        renderEventTrigger(rawData, peakXProcessor, 0.5, 60);
+        
+        renderRawData(rawData, [SENSOR_TYPES.ACCELERATION, DATA_TYPES.X], 0.5, 60 + laneSpacing, "Vertical Accel");
+        renderEventTrigger(rawData, zeroXingProcessor, 0.5, 60 + laneSpacing);
+                
+        //renderRawData(rawData, [SENSOR_TYPES.ACCELERATION, DATA_TYPES.Y], 0.5, 60 + laneSpacing * 2, "Y");
+        //renderRawData(rawData, [SENSOR_TYPES.ACCELERATION, DATA_TYPES.Z], 0.5, 60 + laneSpacing * 3, "Z");
+        
+        renderRawData(rawData, [SENSOR_TYPES.ROTATION, DATA_TYPES.X], 500, 60 + laneSpacing * 4, "X ROT");
+        renderRawData(rawData, [SENSOR_TYPES.ROTATION, DATA_TYPES.Y], 100, 60 + laneSpacing * 5, "Y ROT");
+        renderRawData(rawData, [SENSOR_TYPES.ROTATION, DATA_TYPES.Z], 100, 60 + laneSpacing * 6, "Z ROT", 485);
+
+        alignmentChecker();
+    }
+
+    return {
+        get onActivate(){return onActivate},
+        get onDeactivate(){return onDeactivate},
+        get processors(){return testProcessorArr},
+        get render(){return render}
+    }
+}
+
+
+
+
